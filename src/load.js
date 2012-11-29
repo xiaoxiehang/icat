c9c00be1c7c66eed522c48d43069e9b2814f0fa9
wangwenlong <jn_dream@163.com>
2012-11-29 13:54:44 +0800

    add load.js

---
+++ src/load.js
@@ -0,0 +1,9 @@
+(function(iCat){
+	var doc = document,
+		ohead = doc.head || doc.getElementsByTagName('head')[0],
+		metaAppRef = doc.getElementsByName('appRef')[0];
+	
+	iCat.config = function(cfg){
+		
+	}
+})(ICAT);
